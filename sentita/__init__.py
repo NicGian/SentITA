27481f912e027188e41c58c555eecc03737c27cb
ido <ido@cyabra.com>
2020-08-18 12:37:59 +0300

    preformence

--- sentita/__init__.py
+++ sentita/__init__.py
@@ -14,6 +14,8 @@ import pickle
 import spacy
 import re
 import os
+from sentita.singleton import Singleton
+
 path = os.path.dirname(__file__)
 
 MAX_SEQUENCE_LENGTH = 35
@@ -57,37 +59,40 @@ def create_features(text, maxlen):
 
 
 #================ Model ======================#
-def load_model(MAX_SEQUENCE_LENGTH, EMBEDDING_DIM):
-    max_len = MAX_SEQUENCE_LENGTH
-    emb_dim = EMBEDDING_DIM
-    
-    x1 = Input(shape=(max_len,))
-    w_embed = Embedding(NB_WEMBS+1, emb_dim, input_length=max_len, trainable=False)(x1)
-    w_embed = Dropout(0.5)(Dense(64, activation='relu')(w_embed))
-    h = Conv1D(filters=32, kernel_size=2, padding='valid', activation='relu')(w_embed)
-    h = Bidirectional(LSTM(32, return_sequences=True, recurrent_dropout=0.5), merge_mode='concat')(h)
-    h = AveragePooling1D(pool_size=2, strides=None, padding='valid')(h)
-    h = Bidirectional(LSTM(16, return_sequences=True, recurrent_dropout=0.5), merge_mode='concat')(h)
-    h = AveragePooling1D(pool_size=2, strides=None, padding='valid')(h)
-    h = Flatten()(h)
-    preds_pol = Dense(2, activation='sigmoid')(h)
-    
-    model_pol = Model(inputs=[x1], outputs=preds_pol)
-    model_pol.compile(loss='binary_crossentropy', optimizer='nadam', metrics=['accuracy'])
-    #model_pol.summary()
-    
-    STAMP = 'test_sentita_lstm-cnn_wikiner_v1'
-    #early_stopping = EarlyStopping(monitor='val_loss', patience=7)
-    if 'SENTITA_MODEL_PATH' in os.environ.keys():
-        path_model = os.environ['SENTITA_MODEL_PATH']
-    else:
-        path_model = path
-    bst_model_path = os.path.join(path_model, STAMP + '.h5')
-    #checkpointer = ModelCheckpoint(bst_model_path, save_best_only=True, save_weights_only=True)
-    model_pol.load_weights(bst_model_path)
-    return model_pol
 
+class Model(metaclass=Singleton):
+    def __init__(self, MAX_SEQUENCE_LENGTH, EMBEDDING_DIM):
+        self.load_model(MAX_SEQUENCE_LENGTH, EMBEDDING_DIM)
 
+    def load_model(self, MAX_SEQUENCE_LENGTH, EMBEDDING_DIM):
+        max_len = MAX_SEQUENCE_LENGTH
+        emb_dim = EMBEDDING_DIM
+
+        x1 = Input(shape=(max_len,))
+        w_embed = Embedding(NB_WEMBS+1, emb_dim, input_length=max_len, trainable=False)(x1)
+        w_embed = Dropout(0.5)(Dense(64, activation='relu')(w_embed))
+        h = Conv1D(filters=32, kernel_size=2, padding='valid', activation='relu')(w_embed)
+        h = Bidirectional(LSTM(32, return_sequences=True, recurrent_dropout=0.5), merge_mode='concat')(h)
+        h = AveragePooling1D(pool_size=2, strides=None, padding='valid')(h)
+        h = Bidirectional(LSTM(16, return_sequences=True, recurrent_dropout=0.5), merge_mode='concat')(h)
+        h = AveragePooling1D(pool_size=2, strides=None, padding='valid')(h)
+        h = Flatten()(h)
+        preds_pol = Dense(2, activation='sigmoid')(h)
+
+        model_pol = Model(inputs=[x1], outputs=preds_pol)
+        model_pol.compile(loss='binary_crossentropy', optimizer='nadam', metrics=['accuracy'])
+        #model_pol.summary()
+
+        STAMP = 'test_sentita_lstm-cnn_wikiner_v1'
+        #early_stopping = EarlyStopping(monitor='val_loss', patience=7)
+        if 'SENTITA_MODEL_PATH' in os.environ.keys():
+            path_model = os.environ['SENTITA_MODEL_PATH']
+        else:
+            path_model = path
+        bst_model_path = os.path.join(path_model, STAMP + '.h5')
+        #checkpointer = ModelCheckpoint(bst_model_path, save_best_only=True, save_weights_only=True)
+        model_pol.load_weights(bst_model_path)
+        self.model_pol = model_pol
 
 
 def calculate_polarity(sentences):
@@ -99,9 +104,9 @@ def calculate_polarity(sentences):
 
     test_wemb_idxs = np.reshape(np.array([e[0] for e in X_ctest]), [n_ctest_sents, MAX_SEQUENCE_LENGTH])
 
-    sent_model = load_model(MAX_SEQUENCE_LENGTH, EMBEDDING_DIM)
+    sent_model = Model(MAX_SEQUENCE_LENGTH, EMBEDDING_DIM).model_pol
     preds = sent_model.predict([test_wemb_idxs])
     for i in range(n_ctest_sents):
         results.append(sentences[i] + ' - ' + 'opos: ' + str(preds[i][0]) + ' - oneg: ' + str(preds[i][1]))
-        print(sentences[i],' - opos: ', preds[i][0], ' - oneg: ', preds[i][1])
+        # print(sentences[i],' - opos: ', preds[i][0], ' - oneg: ', preds[i][1])
     return results, preds
